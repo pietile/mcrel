c525149023a6628b7f801a08e0e37826586cf7a3
Maksim Milyutin <mmxckk@gmail.com>
2019-10-11 13:48:33 +0300

    Return same setState after state update

--- src/react/useMcrelState.ts
+++ src/react/useMcrelState.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useCallback, useReducer, useRef } from 'react';
 
 import { DeepReadonlyObject } from '../Store';
 import { DeepMergeAtomics, Subset, deepMerge } from '../Store/deepMerge';
@@ -11,17 +11,23 @@ export function useMcrelState<T>(
     arg: P | ((state: DeepReadonlyObject<T>) => P),
   ) => void,
 ] {
-  const [source, setSource] = useState<T>(state);
+  const source = useRef<T>(state);
+  const [, forceUpdate] = useReducer((x): number => x + 1, 0);
 
-  function setState<P extends Subset<T, P, DeepMergeAtomics<never>>>(
-    arg: P | ((value: DeepReadonlyObject<T>) => P),
-  ): void {
-    if (typeof arg === 'object') {
-      setSource(deepMerge(source, arg));
-    } else {
-      setSource(deepMerge(source, arg(source as DeepReadonlyObject<T>)));
-    }
-  }
+  const setState = useCallback(
+    <P extends Subset<T, P, DeepMergeAtomics<never>>>(
+      arg: P | ((value: DeepReadonlyObject<T>) => P),
+    ): void => {
+      if (typeof arg === 'object') {
+        source.current = deepMerge(source.current, arg);
+      } else {
+        source.current = deepMerge(source.current, arg(source.current as DeepReadonlyObject<T>));
+      }
 
-  return [source as DeepReadonlyObject<T>, setState];
+      forceUpdate({});
+    },
+    [],
+  );
+
+  return [source.current as DeepReadonlyObject<T>, setState];
 }
